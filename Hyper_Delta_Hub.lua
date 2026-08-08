0a781d1b1e644e8a24880630760feff1f0fed1dc
Bohdan <mukhalkivbohdancats@gmail.com>
2026-08-08 14:52:26 +0300

    Update project details and GUI names in script

--- Hyper_Delta_Hub.lua
+++ Hyper_Delta_Hub.lua
@@ -1,8 +1,9 @@
 --[[
     ================================================================
     [ SCRIPT INFORMATION ]
-    Project: Hyper Delta Hub 
-    Author: Bohdancatnapcats50 
+    Project: Custom Script
+    Author: OYB
+    YouTube: https://www.youtube.com/channel/UCAlXXV1Hbvf7WbfXARuVtiQ
     
     [ TERMS AND CONDITIONS ]
     - You ARE allowed to use and modify this script for your own games.
@@ -18,8 +19,11 @@ local Config = {
     ServiceId       = 29223, -- Your PlatoBoost Service ID
     PlatoSecret     = "b2ddae15-1581-41ab-a4f0-8111ead283d6", -- Your PlatoBoost Secret Key
 
+    -- [2] Anti-Bypass / Global Secret Variable
+    Secret          = "Code", -- This makes the script ONLY run from the key script. Even if they copy the original obfuscated script to bypass the key, they won't be able to!
+    
     -- [3] Scripts & Links
-    MainScriptURL   = "https://raw.githubusercontent.com/mukhalkivbohdancats-dev/HyperDeltaHub/refs/heads/main/HyperDeltaHub.lua", -- The raw URL of your main script
+    MainScriptURL   = "Link", -- The raw URL of your main script
     
     -- [4] Social Media Settings (Set to true to show, false to hide)
     ShowDiscord     = false,
@@ -35,12 +39,12 @@ local Config = {
     KeyFileName     = "Mykey.txt", -- The name of the file where the valid key will be saved for auto-login
 
     -- [6] GUI Management
-    OldGuiName      = "Hyper Delta Hub", -- Name of the old GUI to destroy if it's already open
-    MainGuiName     = "Hyper Delta Hub", -- Name of the main script's GUI to check if it's already executing
+    OldGuiName      = "", -- Name of the old GUI to destroy if it's already open
+    MainGuiName     = "", -- Name of the main script's GUI to check if it's already executing
 
     -- [7] Hub Information & UI Text
-    HubName         = "Hyper Delta Hub", -- The main title shown at the top of the GUI
-    HubDescription  = "Script with many functions" -- The text shown below the title
+    HubName         = "Test", -- The main title shown at the top of the GUI
+    HubDescription  = "Test" -- The text shown below the title
 }
 
 -------------------------------------------------------------------------------
